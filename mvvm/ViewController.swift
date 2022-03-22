43623ce340cbb516773fc55cab84163f6f207d98
Apurva Jagtap <apurva.jagtap@coditas.com>
2022-03-22 18:11:27 +0530

    Initial Commit

---
+++ mvvm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by coditas on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func login(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        @IBAction func didTapOnLogin(_ sender: Any)
+    }
+
+
+}
+
